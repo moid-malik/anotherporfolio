e40595907ce8b0519fa53f49c0434b11b0fdd95d
Professor-Dollar <abdulmoid2007@gmail.com>
2024-11-07 22:47:41 +0500

    ui fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="w-full text-white items-center leading-none flex  justify-end px-12 pt-10 fixed z-[100] right-0 top-0 ">
+    <div className=" text-white items-center leading-none flex  justify-end fixed z-[100] right-0 top-0 ">
         <a href="#contact" className="inline-flex h-12 animate-shimmer items-center justify-center rounded-3xl border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-200 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
           Hire Me
         </a>
